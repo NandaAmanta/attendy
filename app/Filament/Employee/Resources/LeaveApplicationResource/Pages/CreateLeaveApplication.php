be33b85a8cc55df9ed1f10a7df7c4c4008e12a3b
I Putu Nanda Amanta <nansamgr@gmail.com>
2024-10-27 12:09:47 +0800

    feat : draft button

--- app/Filament/Employee/Resources/LeaveApplicationResource/Pages/CreateLeaveApplication.php
+++ app/Filament/Employee/Resources/LeaveApplicationResource/Pages/CreateLeaveApplication.php
@@ -2,11 +2,36 @@
 
 namespace App\Filament\Employee\Resources\LeaveApplicationResource\Pages;
 
+use App\Consts\LeaveStatus;
 use App\Filament\Employee\Resources\LeaveApplicationResource;
-use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateLeaveApplication extends CreateRecord
 {
     protected static string $resource = LeaveApplicationResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCreateFormDraftAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getCreateFormDraftAction(): Action
+    {
+        return Action::make('draft')
+            ->label(__('filament-panels::resources/pages/create-record.form.actions.create.label'))
+            ->color('secondary')
+            ->action('createDraft');
+    }
+
+    public function createDraft()
+    {
+        $data = $this->form->getState();
+        $data['status'] = LeaveStatus::DRAFT;
+        $this->handleRecordCreation($data);
+    }
 }
