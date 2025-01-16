7c0b0a668429b5ece2129b67e9def91831beb35b
I Putu Nanda Amanta <nansamgr@gmail.com>
2025-01-16 14:02:19 +0800

    UP readme

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'owner',
+            'email' => 'owner@email.com',
+        ]);
+
+        $this->call([
+            PermissionSeeder::class,
+        ]);
+    }
+}
